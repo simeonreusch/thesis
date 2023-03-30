8168a1e1bc72a9e2c52ef13831ebbe716a71f8b5
simeonreusch <simeon.reusch@desy.de>
2023-03-30 12:23:31 +0200

    more updates

--- code/chapter_4_follow_up/plot_overview.py
+++ code/chapter_4_follow_up/plot_overview.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from matplotlib import rc
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
